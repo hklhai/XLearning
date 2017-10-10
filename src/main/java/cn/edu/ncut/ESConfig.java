8b5022936991ef14be39c71ca90040c7c44e35eb
hklhai <hkhai@outlook.com>
2017-10-10 15:26:37 +0800

    es config

---
+++ src/main/java/cn/edu/ncut/ESConfig.java
@@ -0,0 +1,32 @@
+package cn.edu.ncut;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Ocean lin on 2017/10/10.
+ */
+@Configuration
+public class ESConfig {
+    @Bean
+    public TransportClient client() throws UnknownHostException {
+        InetSocketTransportAddress node1 = new InetSocketTransportAddress(
+                InetAddress.getByName("http://elasticsearch1"), 9300
+        );
+        //可以new 三个节点，将node1放入,还可以放入node2...
+
+        Settings settings = Settings.builder().put("cluster.name", "es-hk").build();
+
+        TransportClient client = new PreBuiltTransportClient(settings);
+        client.addTransportAddress(node1);
+        return client;
+    }
+
+}
